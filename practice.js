c0a40515d7152b37a9d6de2f39d90d4a1cfe4871
Zack Claar <claar.zack@gmail.com>
2015-08-31 11:01:08 -0600

    Done with redoing tip calc

---
+++ practice.js
@@ -0,0 +1,19 @@
+var DistanceObj = function(x1, y1) {
+  this.x1 = x1;
+  this.y1 = y1;
+};
+
+
+DistanceObj.prototype.whoIsClosest = function(playerNode1, playerNode2, playerNode3, playerNode4, playerNode5, flagNode) {
+
+  var p1 = Math.hypot(flagNode.x - playerNode1)
+
+}
+
+p1 = new DistanceObj(0, 1);
+p2 = new DistanceObj(2, 1);
+p3 = new DistanceObj(4, 4);
+p4 = new DistanceObj(4, 2);
+p5 = new DistanceObj(1, 4);
+
+DistanceObj,
