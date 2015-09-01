3e3e5c2d740cdd1a3e137aa63f89a5d097972a9f
Zack Claar <claar.zack@gmail.com>
2015-08-31 23:18:12 -0600

    number changes colors depending on number of votes

--- Unit-1/actualFiles/reddit.js
+++ Unit-1/actualFiles/reddit.js
@@ -4,6 +4,14 @@ app.controller("NavBar", function($scope) {
   // console.log('in nav bar');
   $scope.newPostBool = false;
 
+  $scope.posts = [{
+    title: "Some Title",
+    author: "Some Author",
+    imageURL: "http://i.imgur.com/TcmVKyPb.jpg",
+    description: "Some Description",
+    votes: 0
+  }];
+
   $scope.showNewPostForm = function() {
     console.log('in');
     $scope.newPostBool = true;
@@ -15,6 +23,14 @@ app.controller("NavBar", function($scope) {
 
   $scope.sortPostsBy = function(str) {
     $scope.sortPostsByWhat = str;
+  };
+
+  $scope.changeVotes = function(bool, post) {
+    if(bool === 'down') {
+      post.votes -= 1;
+    } else {
+      post.votes += 1;
+    }
   };
 
 });
