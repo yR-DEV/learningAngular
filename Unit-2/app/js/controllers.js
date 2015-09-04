73b4c1ca19c8151625f32ffab1d62c2fcff7358f
Zack Claar <claar.zack@gmail.com>
2015-09-04 09:44:50 -0600

    I maybe have thid routing thing figured out

---
+++ Unit-2/app/js/controllers.js
@@ -0,0 +1,7 @@
+app.controller('HomeController', function($scope){
+  $scope.greeting = "WELCOMEE!";
+});
+
+app.controller('DogsController', function($scope) {
+  $scope.dogGreeting = "Woof, woof.";
+});
