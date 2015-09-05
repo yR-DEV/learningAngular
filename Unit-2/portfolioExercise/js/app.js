80badcbb60f8be67944984ce4d750b38b8c478c8
Zack Claar <claar.zack@gmail.com>
2015-09-04 18:07:28 -0600

    quick commit, working on the portfolio website exercise and have all the routes working

--- Unit-2/portfolioExercise/js/app.js
+++ Unit-2/portfolioExercise/js/app.js
@@ -5,35 +5,13 @@ app.config(function($routeProvider) {
     .when('/bio', {
       templateUrl: "partials/bio.html",
       controller: "BioController"
+    })
+    .when('/projects', {
+      templateUrl: "partials/projects.html",
+      controller: "ProjectsController"
+    })
+    .when('/resume', {
+      templateUrl: "partials/resume.html",
+      controller: "ResumeController"
     });
 });
-
-
-// app.config(function($routeProvider) {
-//   $routeProvider
-//     .when('/',{
-//       templateUrl: 'index.html',
-//       controller: 'IndexController'
-//     })
-//     .when('/bio', {
-//       templateUrl: 'partials/bio.html',
-//       controller: 'BioController'
-//     });
-// });
-
-
-// app.config(function($routeProvider) {
-//   $routeProvider.when('/projects', {
-//     templateUrl: 'partials/projects.html',
-//     controller: 'ProjectsController'
-//   })
-//     .when('/bio', {
-//       templateUrl: 'partials/bio.html',
-//       controller: 'BioController'
-//   })
-//     .when('/resume', {
-//       templateUrl: 'partials/resume.html',
-//       controller: 'ResumeController'
-//     });
-//
-// });
--- Unit-2/portfolioExercise/js/controllers.js
+++ Unit-2/portfolioExercise/js/controllers.js
@@ -1,3 +1,11 @@
 app.controller("BioController", function($scope) {
   $scope.bio = "About Me";
 });
+
+app.controller("ProjectsController", function($scope) {
+  $scope.projects = "PROJECTZ!";
+});
+
+app.controller("ResumeController", function($scope) {
+  $scope.resume = "MY STUFF";
+});
