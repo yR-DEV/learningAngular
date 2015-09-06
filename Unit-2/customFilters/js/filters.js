2d1a60f9f5fb54c40cce081b14aed4d7295a3adc
Zack Claar <claar.zack@gmail.com>
2015-09-06 11:10:55 -0600

    Got the custom camel filter working for both snake and kebab strings

--- Unit-2/customFilters/js/filters.js
+++ Unit-2/customFilters/js/filters.js
@@ -7,3 +7,35 @@ app.filter('kebab', function() {
     }
   };
 });
+
+app.filter('camel', function() {
+  var tempInput = [];
+  var dashIndex;
+  var tempArr = [];
+  var tempLetter = "";
+
+  return function(input) {
+    tempInput = input.split('');
+    tempInput.forEach(function(letter, i) {
+      if(letter === '-' || letter === '_') {
+        // console.log(letter);
+        // tempInput.slice(i, 1);
+        dashIndex = i;
+        // console.log(dashIndex);
+      } else if(i === dashIndex + 1) {
+        console.log('next letter');
+        tempLetter = tempInput[i].toUpperCase();
+        tempArr.push(tempLetter);
+      } else {
+        console.log(tempInput[i]);
+        tempLetter = tempInput[i].toLowerCase();
+        tempArr.push(tempLetter);
+      }
+
+
+    });
+    var finalArr = tempArr.join('');
+    // console.log(tempArr);
+    return finalArr;
+  };
+});
