76d223bc5f068c706a2f46ae26e2e2cb9b1abec9
Zack Claar <claar.zack@gmail.com>
2015-09-06 10:35:21 -0600

    Can check whether input for the filter is a string or num and change output accordingly

--- Unit-2/customFilters/js/filters.js
+++ Unit-2/customFilters/js/filters.js
@@ -1,5 +1,9 @@
 app.filter('kebab', function() {
   return function(input) {
-    return input.replace(/_/g, "-");
+    if(typeof input === 'string') {
+      return input.replace(/_/g, "-");
+    } else {
+      return "ONLY LETTERS PLS"
+    }
   };
 });
