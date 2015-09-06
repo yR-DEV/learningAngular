8f7c8e41e737e63ba4444bdb2b9cceb26fa350b9
Zack Claar <claar.zack@gmail.com>
2015-09-06 10:30:45 -0600

    Got my first filter working! kebab! angular is so freaking cool

--- Unit-2/customFilters/js/filters.js
+++ Unit-2/customFilters/js/filters.js
@@ -1,10 +1,5 @@
 app.filter('kebab', function() {
   return function(input) {
-    if(typeof(input) !== string) {
-      console.log('not a string');
-      return false;
-    } else {
-      return input.replace(/_/g, "-");
-    }
+    return input.replace(/_/g, "-");
   };
 });
