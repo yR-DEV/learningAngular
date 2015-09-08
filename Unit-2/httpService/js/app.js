00fcece4e51c61ea831b2d3fb3d90ded29fba7e3
Zack Claar <claar.zack@gmail.com>
2015-09-08 12:51:24 -0600

    I will get help with this later, going to move on because I am getting so frusterated with this

--- Unit-2/httpService/js/app.js
+++ Unit-2/httpService/js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module("HttpService", []);
+
+app.config(['$httpProvider', function($httpProvider) {
+
+        $httpProvider.defaults.useXDomain = true;
+
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    }
+
+]);
