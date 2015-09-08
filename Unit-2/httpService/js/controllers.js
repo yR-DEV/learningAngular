861dcbe64fd425e001f7df6a3c1639223d93ca6c
Zack Claar <claar.zack@gmail.com>
2015-09-08 11:18:00 -0600

    connecting and getting data from the rails api

--- Unit-2/httpService/js/controllers.js
+++ Unit-2/httpService/js/controllers.js
@@ -1,7 +1,12 @@
 app.controller('HttpController', function($scope, $http) {
 
-  $http.get('https://itunes.apple.com/search?term=jack+johnson').then(function(wisdom) {
-    return $scope.zenWisdom = wisdom.data;
+  // $http.get('https://itunes.apple.com/search?term=jack+johnson').then(function(wisdom) {
+  //   return $scope.zenWisdom = wisdom.data;
+  // });
+
+  $http.get('https://shielded-peak-6345.herokuapp.com/').then(function(apiData) {
+    console.log(apiData.data);
+    $scope.data = apiData.data;
   });
 
 });
