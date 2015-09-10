b1541ec426a4983af502e5af0a82bdd131e16a68
Zack Claar <claar.zack@gmail.com>
2015-09-10 10:19:28 -0600

    Still cannot figure out how to get this factory working

--- Unit-2/omdbSearch/js/controllers.js
+++ Unit-2/omdbSearch/js/controllers.js
@@ -1,6 +1,20 @@
-app.controller('SearchController', ['$scope',
-function($scope) {
-  // console.log('in controller');
-  // $scope.movieQuery = MovieQuery.movieQuery;
-  $scope.greeting = "Hello.";
+// app.controller('SearchController', ['$scope',
+// function($scope) {
+//   // console.log('in controller');
+//   // $scope.movieQuery = MovieQuery.movieQuery;
+//   $scope.greeting = "Hello.";
+// }]);
+
+// app.controller('SearchController', ['$scope', '$http', 'movieQuery',
+// function($scope, $http, movieQuery) {
+//   // $scope.callMovieQuery = function(movieTitleInput) {
+//   //   MovieQuery(movieTitleInput);
+//   // };
+//   console.log('in search controller');
+// }]);
+
+app.controller('SearchController', ['$scope', '$http', 'movieQuery',
+function($scope, $http, movieQuery) {
+  $scope.results = movieQuery.queryMatches();
+  console.log('in search controller');
 }]);
--- Unit-2/omdbSearch/js/services.js
+++ Unit-2/omdbSearch/js/services.js
@@ -1,4 +1,10 @@
-
-app.factory('MovieQuery', function() {
-  console.log('in factory');
-});
+app.factory('movieQuery', ['$scope', '$http',
+function($scope, $http) {
+  return {
+    queryMatches: function() {
+      var x = "hello";
+      return x;
+    }
+  };
+  // console.log('in factory');
+}]);
