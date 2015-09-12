a04e7b3acbf9e0db98f93c01718336bec3e4ebb5
Zack Claar <claar.zack@gmail.com>
2015-09-12 13:52:52 -0600

    In the product controller

--- Unit-2/meanTea/js/controller.js
+++ Unit-2/meanTea/js/controller.js
@@ -2,3 +2,9 @@ app.controller('SearchController', ['$scope', '$http',
 function($scope, $http) {
   console.log('in search controller');
 }]);
+
+
+app.controller('ProductController', ['$scope', '$http',
+function($scope, $http) {
+  console.log('in product controller');
+}]);
